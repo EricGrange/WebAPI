b4319afb4b3816e15e2cf041663605400ae8e7c4
Eric Grange <egrange@creative-it.net>
2016-02-03 09:35:33 +0100

    Extended JDate

--- WebAPI.pas
+++ WebAPI.pas
@@ -21,10 +21,20 @@ type
 
    JDate = class external 'Date'
 
-      constructor Create(milliseconds : Integer); default;
+      constructor Create(); overload; external;
+      constructor Create(millisecondsSinceEpoch : Float); overload; external;
 
+      function GetMinutes : Integer; external 'getMinutes';
+      function GetHours : Integer; external 'getHours';
+
+      function GetDate : Integer; external 'getDate'; // 1-31
+      function GetMonth : Integer; external 'getMonth'; // 0-11
+      function GetFullYear : Integer; external 'getFullYear'; // 2016
+
+      function GetTime : Float; external 'getTime';
+      class function Now : Float; external 'now';
+      
    end;
-
 
 function JSON : JJSON; external 'JSON' property;
 
@@ -34,7 +44,6 @@ function Window : Variant; external 'window' property;
 function Console : Variant; external 'console' property;
 function Math : Variant; external 'Math' property;
 function LocalStorage : Variant; external 'localStorage' property;
-
 procedure Alert(msg : String); external 'alert';
 
 function SetTimeout(proc : procedure; delayMilliseconds : Integer) : Integer; external 'setTimeout';
